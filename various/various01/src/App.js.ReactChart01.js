a02d22a0b6eee75454967dba67707b2b54abafd1
coderdba <gowrishdba@yahoo.com>
2021-02-26 18:21:46 +0530

    more changes

--- various/various01/src/App.js.ReactChart01.js
+++ various/various01/src/App.js.ReactChart01.js
@@ -3,6 +3,7 @@ import React from 'react';
 import LineExample from './ReactChart01/components/line';
 import LineMulti01 from './ReactChart01/components/lineMulti01';
 import LineMulti02c from './ReactChart01/components/lineMulti02c';
+import LineMulti02cParamCaller from './ReactChart01/components/lineMulti02cParamCaller.js';
 
 export default class App extends React.Component {
 	render() {
@@ -12,6 +13,7 @@ export default class App extends React.Component {
 				<LineExample />
 				<LineMulti01 />
 				<LineMulti02c />
+				<LineMulti02cParamCaller />
 			</div>
 		);
 	}
